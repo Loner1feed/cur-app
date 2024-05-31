601dade2f0d2c63d5b966a2dbf327875f0c4093e
Eugrn Guriev <lfeedwork05@gmail.com>
2024-05-31 14:03:59 +0200

    fixed api

--- src/api/currencyAPI.js
+++ src/api/currencyAPI.js
@@ -2,7 +2,11 @@ import { instance } from "./instance"
 
 export const currencyAPI = {
   getCurrencies: async () => {
-    const res = instance.get(`/latest?symbols=USD,EUR,UAH&base=USD`)
+    const res = instance.get(`/latest?symbols=USD,EUR,UAH&base=USD`, {
+      headers: {
+        "apikey": "hjdEDynnE3xbf3wOURgucA1zCZMzPH76"
+      }
+    })
     return res;
   },
 };
--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const host = 'https://api.exchangerate.host'
+const host = 'https://api.apilayer.com/exchangerates_data'
 
 export const instance = axios.create({
   baseURL: `${host}`,
